6e3366fb9731e1a7dd7be1ac75c0bf383f32b3d5
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-09-01 10:23:47 +0300

    Add entry model dict.

---
+++ app/model.py
@@ -0,0 +1,26 @@
+entry_types = \
+            {"silt_active_ml_per_l":
+             {"finnish": "Aktiivilietemittaus", "max": 0, "min": 1000,
+              "input-type":  "numeric", "unit": "ml per litra",
+              "description": "Aktiivilietteen mittaus"},
+             "silt_surplus_removal_l":
+             {"finnish": "Poistopumppaus", "max": 1, "min": 1000,
+              "input-type":  "numeric", "unit": "litraa",
+              "description": "Ylijäämälietteen poistomäärä litroina."},
+             "pump_usage_hours":
+             {"finnish": "Pumpun käyttötunnit", "max": 1, "min": 9999999,
+              "input-type": "numeric", "unit": "tuntia",
+              "description": "Pumpun käyttötuntilaskurin lukema"},
+             "water_quality":
+             {"finnish": "Kirkasvesinäyte", "max": 1, "min": 3,
+              "input-type": "numeric", "unit": "1-3",
+              "description":
+              "Kirkasveden laatu asteikolla 1-3 (1 on parhain)"},
+             "ferrosulphate_level_percent":
+             {"finnish": "Ferrosulfaatin määrä", "max": 0, "min": 100,
+              "input-type":  "numeric", "unit": "prosenttiluku",
+              "description": "Ferrosulfaattimittarin prosenttilukema"},
+             "ferrosulphate_addition_kg":
+             {"finnish": "Ferrosulfaatin lisäys", "max": 1, "min": 1000,
+              "input-type":  "numeric", "unit": "kiloa", "description":
+              "Ferrosulfaatin määrän lisäys kiloina."}}
