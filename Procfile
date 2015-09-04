bb04e6bcc9b7d1f5bf410c9ffd50bb02b5e7a61a
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-09-04 15:53:55 +0300

    Add db_init to Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+initializer: python app/server.py init_db
 web: python app/run.py
--- app/run.py
+++ app/run.py
@@ -3,12 +3,10 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from server import app
-from model import InitDb
+
 import logging
 from logging import StreamHandler
 import os
-
-InitDb()
 
 file_handler = StreamHandler()
 file_handler.setLevel(logging.WARNING)
