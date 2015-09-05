8ca96f94d3ae59c29de160f0060813a9bc652047
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-09-05 16:27:07 +0300

    Fix script errors, update readme.

--- scripts/heroku-caffeine.py
+++ scripts/heroku-caffeine.py
@@ -2,14 +2,15 @@
 # This script keeps the free Heroku dyno awake during daytime.
 # During night, user needs to wait ~10s for server wakeup.
 
-# Tested on with Python 2.7 and Python 3.4. Depends on UNIX.
+# Tested on Python 2.7.6, should work on *nix machines.
+# (On Python 3 urllib2 is changed to urllib3..)
 
 # Configuration & Usage:
-# - Setup server endpoints in KEEP_AWAKE_SERVERS env var, separated by ':'.
+# - Setup server endpoints in KEEP_AWAKE_SERVERS env var, separated by space.
+#   eg. `export KEEP_AWAKE_SERVERS="http://app955.herokuapp.com http://asd.me"`
 # - Schedule script with crontab, eg. run every 30 min:
 #   0,30 * * * * python path/to/heroku-caffeine.py
 # - Customize script timezone and sleeping hours to your liking
-
 ###
 
 import os
@@ -24,21 +25,22 @@ import urllib2
 # Valid timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 os.environ['TZ'] = 'Europe/Helsinki'                    # <- Customize timezone
 time.tzset()
-time.now()
+
 now = datetime.now()
 
 sleeping_hours = [22, 23, 0, 1, 2, 3, 4, 5]  # <- Customize dyno sleeping hours
 
 if (now.hour in sleeping_hours):
     print("Letting Heroku server sleep, time is " + time.strftime('%X %x %Z'))
-    return
+    exit(0)
 
 
 def wakeup_server(url):
-    print("Waking up " + url)
-    response = urllib2.urlopen(os.environ['KEEP_AWAKE_SERVERS'])
+    print("Waking up " + url + " at " + time.strftime('%X %x %Z'))
+    response = urllib2.urlopen(url)
     return response.read()
 
 # os.environ['KEEP_AWAKE_SERVERS'] = 'testing:stuff'
-servers = os.environ['KEEP_AWAKE_SERVERS'].split(':')
+servers = os.environ['KEEP_AWAKE_SERVERS'].split()
 map(lambda url: wakeup_server(url), servers)
+exit(0)
